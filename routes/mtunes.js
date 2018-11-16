1067b51c245a964760de8c8ac76118ab8665f6a2
aruntyagi2018 <aruntyagi2005@gmail.com>
2018-11-16 22:37:10 +0400

    my changes

--- routes/mtunes.js
+++ routes/mtunes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/:searchtxt',function(req,res){
-    console.log('route called : ' + req.params.searchtxt);
+    //console.log('route called : ' + req.params.searchtxt);
     request.get('https://itunes.apple.com/search?term=' + req.params.searchtxt,function(err,response,body){
        res.send(body);
        //res.json({searchData:{mdata:'mtunes my data',count:12}});
